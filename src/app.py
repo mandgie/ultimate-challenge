947bd4bed4b1a6856ffb7e9d47e86da924e3d1c0
Magnus Friberg <mfriberg@spotify.com>
2019-12-18 19:40:22 +0100

    Added new login page

--- src/app.py
+++ src/app.py
@@ -10,6 +10,10 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/login_new")
+def login_new():
+    return render_template("login_new.html")
+
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
